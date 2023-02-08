fc94b50f176571cfdbfe716a759e168579c47b40
ishaksebsib <ishaksebsib@gmail.com>
2023-02-08 23:14:37 +0300

    Connect MongoDB

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    mongoose.set("strictQuery", true);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDb connecte ${conn.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const colors = require("colors");
 const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT;
-
 const app = express();
+
+connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
