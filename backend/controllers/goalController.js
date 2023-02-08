a7cf870940c897319f94b41ec75bc38e70c24c10
ishaksebsib <ishaksebsib@gmail.com>
2023-02-08 22:12:22 +0300

    error middle ware and express async handler

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,30 +1,41 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
-const getGoals = (req, res) => {
+
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "get goals" });
-};
+});
 
 // @desc    Set goal
 // @route   POST /api/goals
 // @access  Private
-const setGoal = (req, res) => {
+
+const setGoal = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Plase add text field");
+  }
+
   res.status(200).json({ message: "create goals" });
-};
+});
 
 // @desc    Update goal
 // @route   PUT /api/goals/:id
 // @access  Private
-const updateGoal = (req, res) => {
+
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "update goals" });
-};
+});
 
 // @desc    Delete goal
 // @route   DELETE /api/goals/:id
 // @access  Private
-const deleteGoal = (req, res) => {
+
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "delte goals" });
-};
+});
 
 module.exports = {
   getGoals,
